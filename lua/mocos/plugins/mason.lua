158776c1641911216a27d7c816a93d04aa244718
mocos <130593875+MoXcz@users.noreply.github.com>
2024-12-19 00:45:33 -0500

    Add mason ensure install for formatters

--- lua/mocos/plugins/mason.lua
+++ lua/mocos/plugins/mason.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     'williamboman/mason-lspconfig.nvim',
     'olrtg/nvim-emmet',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
   config = function()
     -- Emmet abbreviations
@@ -13,6 +14,9 @@ return {
     mason.setup({})
     mason_lspconfig.setup({
       ensure_installed = { 'lua_ls', 'rust_analyzer', 'html' },
+    })
+    require('mason-tool-installer').setup({
+      ensure_installed = { 'prettier', 'google-java-format', 'black', 'clangd' },
     })
   end,
 }
