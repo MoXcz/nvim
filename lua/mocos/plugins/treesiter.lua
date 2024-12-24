2bededbcefb9acc8e1f2f9657b61d501bb609fdd
mocos <130593875+MoXcz@users.noreply.github.com>
2024-12-24 13:52:25 -0500

    Testing treesitter text objects

--- lua/mocos/plugins/treesiter.lua
+++ lua/mocos/plugins/treesiter.lua
@@ -1,6 +1,8 @@
+---@diagnostic disable: missing-fields
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
@@ -17,6 +19,54 @@ return {
             end
           end,
           additional_vim_regex_highlighting = false,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              ['ii'] = '@conditional.inner',
+              ['ai'] = '@conditional.outer',
+              ['il'] = '@loop.inner',
+              ['al'] = '@loop.outer',
+              ['at'] = '@comment.outer',
+            },
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']f'] = '@function.outer',
+            [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+            [']F'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[f'] = '@function.outer',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[F'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>ac'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<leader>Ac'] = '@parameter.inner',
+          },
         },
       })
     end,
