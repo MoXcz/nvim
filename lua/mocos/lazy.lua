2d0c318111a5ef34e53b87b38920f50c72af554c
mocos <130593875+MoXcz@users.noreply.github.com>
2025-02-03 13:08:33 -0500

    Add todo highlights

--- lua/mocos/lazy.lua
+++ lua/mocos/lazy.lua
@@ -39,5 +39,12 @@ require('lazy').setup({
         vim.cmd.hi('Comment gui=none')
       end,
     },
+    -- Highlight to do's
+    {
+      'folke/todo-comments.nvim',
+      event = 'VimEnter',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      opts = { signs = false },
+    },
   },
 })
