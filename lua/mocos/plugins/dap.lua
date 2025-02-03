bfb2e00d4b42f6cc257a64c3e056c6cbfc476246
mocos <130593875+MoXcz@users.noreply.github.com>
2025-02-03 13:09:07 -0500

    Add debugger (wip)

--- lua/mocos/plugins/dap.lua
+++ lua/mocos/plugins/dap.lua
@@ -1,25 +1,128 @@
 return {
-  'rcarriga/nvim-dap-ui',
+  'mfussenegger/nvim-dap',
   dependencies = {
-    'mfussenegger/nvim-dap',
+    -- Add UI
+    'rcarriga/nvim-dap-ui',
     'nvim-neotest/nvim-nio',
-    'theHamsta/nvim-dap-virtual-text',
     'leoluz/nvim-dap-go',
+    -- Installs the debug adapters automatically
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+  },
+  keys = {
+    -- Basic debugging keymaps, feel free to change to your liking!
+    {
+      '<F5>',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Debug: Start/Continue',
+    },
+    {
+      '<F1>',
+      function()
+        require('dap').step_into()
+      end,
+      desc = 'Debug: Step Into',
+    },
+    {
+      '<F2>',
+      function()
+        require('dap').step_over()
+      end,
+      desc = 'Debug: Step Over',
+    },
+    {
+      '<F3>',
+      function()
+        require('dap').step_out()
+      end,
+      desc = 'Debug: Step Out',
+    },
+    {
+      '<leader>b',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'Debug: Toggle Breakpoint',
+    },
+    {
+      '<leader>B',
+      function()
+        require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
+      end,
+      desc = 'Debug: Set Breakpoint',
+    },
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    {
+      '<F7>',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = 'Debug: See last session result.',
+    },
   },
   config = function()
-    require('dapui').setup()
-    require('dap-go').setup()
-    require('nvim-dap-virtual-text').setup()
-    vim.fn.sign_define(
-      'DapBreakpoint',
-      { text = '🔴', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' }
-    )
+    local dap = require('dap')
+    local dapui = require('dapui')
 
-    -- Debugger
-    vim.api.nvim_set_keymap('n', '<leader>dt', ':DapUiToggle<CR>', { noremap = true })
-    vim.api.nvim_set_keymap('n', '<leader>db', ':DapToggleBreakpoint<CR>', { noremap = true })
-    vim.api.nvim_set_keymap('n', '<leader>dc', ':DapContinue<CR>', { noremap = true })
-    vim.api.nvim_set_keymap('n', '<leader>dr', ":lua require('dapui').open({reset = true})<CR>", { noremap = true })
-    vim.api.nvim_set_keymap('n', '<leader>ht', ":lua require('harpoon.ui').toggle_quick_menu()<CR>", { noremap = true })
+    require('mason-nvim-dap').setup({
+      -- Set debuggers with reasonable configuration
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {
+        'delve',
+        'codelldb',
+      },
+    })
+
+    -- Change breakpoint icons
+    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+    vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+    local breakpoint_icons = vim.g.have_nerd_font
+        and {
+          Breakpoint = '',
+          BreakpointCondition = '',
+          BreakpointRejected = '',
+          LogPoint = '',
+          Stopped = '',
+        }
+      or {
+        Breakpoint = '●',
+        BreakpointCondition = '⊜',
+        BreakpointRejected = '⊘',
+        LogPoint = '◆',
+        Stopped = '⭔',
+      }
+    for type, icon in pairs(breakpoint_icons) do
+      local tp = 'Dap' .. type
+      local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    -- Install golang specific config
+    require('dap-go').setup({
+      delve = {
+        -- On Windows delve must be run attached or it crashes.
+        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+        detached = vim.fn.has('win32') == 0,
+      },
+    })
   end,
 }
