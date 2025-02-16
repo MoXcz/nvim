6aa853960c7c76c3b5698ec2f846ba5cafdc522d
mocos <130593875+MoXcz@users.noreply.github.com>
2025-02-16 02:31:07 -0500

    Remove telescope and lsp keymaps
    They are moved to snacks

--- lua/mocos/plugins/lsp.lua
+++ lua/mocos/plugins/lsp.lua
@@ -11,26 +11,15 @@ return {
         callback = function()
           local key = vim.keymap.set
           key('n', '<leader>rd', ':LspStop<CR>')
-          key('n', 'gd', require('telescope.builtin').lsp_definitions)
-          key('n', 'gD', vim.lsp.buf.declaration)
-          key('n', 'grr', require('telescope.builtin').lsp_references)
-          key('n', 'gi', require('telescope.builtin').lsp_implementations)
-          key('n', '<leader>gt', require('telescope.builtin').lsp_type_definitions)
-          --  Symbols are things like variables, functions, types, etc.
-          key('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols)
-          key('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
           -- Rename the variable under your cursor.
           key('n', 'grn', vim.lsp.buf.rename)
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           key({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action)
-          key('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>')
           key('n', '<leader>dl', vim.diagnostic.open_float)
           key('n', '[d', vim.diagnostic.goto_prev)
           key('n', ']d', vim.diagnostic.goto_next)
           key('n', 'K', vim.lsp.buf.hover)
-          key('n', '<leader>rs', ':LspRestart<CR>')
-          key('n', '<leader>rd', ':LspStop<CR>')
         end,
       })
 
@@ -44,7 +33,7 @@ return {
         ['emmet_ls'] = function()
           lspconfig['emmet_ls'].setup({
             capabilities = capabilities,
-            filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'svelte' },
+            filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'svelte', 'php' },
           })
         end,
         ['rust_analyzer'] = function() end,
