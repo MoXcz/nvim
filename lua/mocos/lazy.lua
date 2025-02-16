3dc4c84bddf6012efd662242ae2b6b83fcbc8d05
mocos <130593875+MoXcz@users.noreply.github.com>
2025-02-16 02:31:39 -0500

    Add nvim colorizer for colors

--- lua/mocos/lazy.lua
+++ lua/mocos/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
+      { out,                            'WarningMsg' },
       { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
@@ -46,5 +46,11 @@ require('lazy').setup({
       dependencies = { 'nvim-lua/plenary.nvim' },
       opts = { signs = false },
     },
+    {
+      'norcalli/nvim-colorizer.lua',
+      config = function()
+        require("colorizer").setup()
+      end
+    }
   },
 })
