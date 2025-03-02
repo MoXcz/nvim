5f7860125526e34e251bf7413477d508719aa98a
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-02 12:46:47 -0500

    Update mini plugin
    Include support for comments in tsx files

--- lua/mocos/plugins/mini.lua
+++ lua/mocos/plugins/mini.lua
@@ -1,16 +1,92 @@
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
-  config = function()
-    -- Better Around/Inside textobjects
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup({ n_lines = 500 })
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
-  end,
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup({ n_lines = 500 })
+
+      require('mini.files').setup({
+        mappings = {
+          go_in = "<CR>", -- Map both Enter and L to enter directories or open files
+          go_in_plus = "L",
+          go_out = "-",
+          go_out_plus = "H",
+        },
+      })
+      local MiniFiles = require("mini.files")
+      vim.keymap.set("n", "<leader>ee", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" }) -- toggle file explorer
+      vim.keymap.set("n", "<leader>ef", function()
+        MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+        MiniFiles.reveal_cwd()
+      end, { desc = "Toggle into currently opened file" })
+
+      require('mini.splitjoin').setup({
+        mappings = { toggle = 'sj' },
+      })
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - saw) - [S]urround [A]dd [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup({
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+          -- Add custom surroundings to be used on top of builtin ones. For more
+          -- information with examples, see `:h MiniSurround.config`.
+          custom_surroundings = nil,
+
+          -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+          highlight_duration = 300,
+
+          -- Number of lines within which surrounding is searched
+          n_lines = 20,
+
+          -- Whether to respect selection type:
+          -- - Place surroundings on separate lines in linewise mode.
+          -- - Place surroundings on each line in blockwise mode.
+          respect_selection_type = false,
+
+          -- How to search for surrounding (first inside current line, then inside
+          -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+          -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+          -- see `:h MiniSurround.config`.
+          search_method = 'cover',
+
+          -- Whether to disable showing non-error feedback
+          silent = false,
+        },
+      })
+    end,
+  },
+  {
+    'echasnovski/mini.comment',
+    version = false,
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      -- disable the autocommand from ts-context-commentstring
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false,
+      }
+
+      require("mini.comment").setup {
+        -- tsx, jsx, html , svelte comment support
+        options = {
+          custom_commentstring = function()
+            return require('ts_context_commentstring.internal').calculate_commentstring({
+                  key =
+                  'commentstring'
+                })
+                or vim.bo.commentstring
+          end,
+        },
+      }
+    end
+  },
 }
