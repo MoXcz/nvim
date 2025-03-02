f876502be347bf014df55c39f59cb8c5ca3d76c0
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-02 12:45:36 -0500

    Update Obsidian plugin (workflow similar to Obsidian desktop)

--- lua/mocos/plugins/obsidian.lua
+++ lua/mocos/plugins/obsidian.lua
@@ -3,7 +3,7 @@ return {
     'MeanderingProgrammer/markdown.nvim',
     main = 'render-markdown',
     opts = {},
-    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    name = 'render-markdown',                                                            -- Only needed if you have another plugin named markdown.nvim
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you use the mini.nvim suite
   },
 
@@ -12,9 +12,7 @@ return {
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = true,
     ft = 'markdown',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
+    dependencies = { 'nvim-lua/plenary.nvim', },
     config = function()
       require('obsidian').setup({
         workspaces = {
@@ -23,6 +21,7 @@ return {
             path = '~/Notes',
           },
         },
+        ui = { enable = false },
         notes_subdir = '01 - Notes',
         disable_frontmatter = true,
         new_notes_location = 'notes_subdir',
@@ -32,11 +31,17 @@ return {
           time_format = '%H:%M',
           -- A map for custom variables, the key should be the variable and the value a function
           substitutions = {},
+          mappings = { ["<leader>ch"] = "" }
         },
+        note_id_func = function(title)
+          if title ~= nil then
+            return title               -- Use the tag name as the filename
+          else
+            return tostring(os.time()) -- Fallback: use timestamp if no title is found
+          end
+        end,
+        vim.keymap.set("n", "<leader>R", ":ObsidianRename ", { noremap = true, silent = false })
       })
-      note_id_func = function(title)
-        return title
-      end
     end,
   },
 }
