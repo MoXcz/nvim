f067e4600b18404c840a3a42638a813dc3fc737f
mocos <130593875+MoXcz@users.noreply.github.com>
2025-04-04 00:02:25 -0400

    Enable dashboard snacks

--- lua/mocos/plugins/snacks.lua
+++ lua/mocos/plugins/snacks.lua
@@ -21,6 +21,15 @@ return {
     gitbrowse = { enabled = true },
     git = { enabled = true },
     bufdelete = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
   },
   keys = {
     -- Scratch
