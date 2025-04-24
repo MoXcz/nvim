37a9176cd95b8aab033cc0c91d91408432e505be
mocos <130593875+MoXcz@users.noreply.github.com>
2025-04-24 12:37:29 -0400

    chore: Remove mini files (use Snacks explorer)

--- lua/mocos/plugins/mini.lua
+++ lua/mocos/plugins/mini.lua
@@ -9,21 +9,6 @@ return { -- Collection of various small independent plugins/modules
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup({ n_lines = 500 })
 
-      require('mini.files').setup({
-        mappings = {
-          go_in = "<CR>", -- Map both Enter and L to enter directories or open files
-          go_in_plus = "L",
-          go_out = "-",
-          go_out_plus = "H",
-        },
-      })
-      local MiniFiles = require("mini.files")
-      vim.keymap.set("n", "<leader>ee", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" }) -- toggle file explorer
-      vim.keymap.set("n", "<leader>ef", function()
-        MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-        MiniFiles.reveal_cwd()
-      end, { desc = "Toggle into currently opened file" })
-
       require('mini.splitjoin').setup({
         mappings = { toggle = 'sj' },
       })
