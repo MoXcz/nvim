525756d72e240640ea4e98dabba3335e6a827b51
mocos <130593875+MoXcz@users.noreply.github.com>
2025-06-01 11:17:23 -0600

    chore: Remove unused plugins

--- lua/mocos/plugins/testing.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "vim-test/vim-test",
-  dependencies = { "preservim/vimux" },
-  config = function()
-    vim.keymap.set("n", "<leader>tT", ":TestFile<CR>")
-    vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>")
-    vim.cmd("let test#strategy = 'neovim'")
-  end
-}
