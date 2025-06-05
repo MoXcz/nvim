6e571b526f63c735fd85efc7a47865d413a4c2e9
mocos <130593875+MoXcz@users.noreply.github.com>
2025-06-04 20:51:21 -0600

    chore: Add example on new LSP config setup

--- lua/mocos/plugins/lsp.lua
+++ lua/mocos/plugins/lsp.lua
@@ -1,3 +1,14 @@
 return {
   'neovim/nvim-lspconfig',
+  -- Example on how to configure LSPs in >+0.11
+  vim.lsp.config('emmet_ls', {
+    filetypes = { 'html', 'css', 'javascript', 'typescript', 'vue' },
+    init_options = {
+      html = {
+        options = {
+          showSuggestionsAsSnippets = true,
+        },
+      },
+    },
+  }),
 }
