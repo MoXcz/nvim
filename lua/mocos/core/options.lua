f3efd38c598e26af7c2a8c348e39cbc4ad568dde
mocos <130593875+MoXcz@users.noreply.github.com>
2025-06-04 20:51:58 -0600

    feat: Modify warning float windows and default signs
    - <C-w><C-d> for float windows

--- lua/mocos/core/options.lua
+++ lua/mocos/core/options.lua
@@ -90,7 +90,23 @@ set.splitright = true
 
 vim.diagnostic.config({
   virtual_text = true, -- Show diagnostics inline
-  signs = true,
   underline = true,
   update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.INFO] = "󰋽 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
+    },
+  },
 })
