569ff8c86346e9a4fc3967d8dbcb9028156f904e
mocos <130593875+MoXcz@users.noreply.github.com>
2025-07-26 15:05:54 -0600

    feat(lsp): add kulala.nvim (rest client) and clean lazy.lua

--- lua/mocos/lazy.lua
+++ lua/mocos/lazy.lua
@@ -32,7 +32,7 @@ require('lazy').setup({
     },
     { 'rose-pine/neovim' },
     { 'sainnhe/gruvbox-material' },
-    { 'catppuccin/nvim', name = 'catppuccin' },
+    { 'catppuccin/nvim',         name = 'catppuccin' },
     -- Highlight to do's
     {
       'folke/todo-comments.nvim',
@@ -40,49 +40,34 @@ require('lazy').setup({
       dependencies = { 'nvim-lua/plenary.nvim' },
       opts = { signs = false },
     },
+    -- Look at that -> #0a4
     {
       'norcalli/nvim-colorizer.lua',
       config = function()
         require('colorizer').setup()
       end,
     },
+    -- Zen mode
     {
       'folke/zen-mode.nvim',
-      opts = {},
     },
     {
       'folke/twilight.nvim',
-      opts = {},
     },
     {
-      'jbyuki/venn.nvim',
+      "mbbill/undotree",
       config = function()
-        -- venn.nvim: enable or disable keymappings
-        function _G.Toggle_venn()
-          local venn_enabled = vim.inspect(vim.b.venn_enabled)
-          if venn_enabled == 'nil' then
-            vim.b.venn_enabled = true
-            vim.cmd([[setlocal ve=all]])
-            -- draw a line on HJKL keystokes
-            vim.api.nvim_buf_set_keymap(0, 'n', 'J', '<C-v>j:VBox<CR>', { noremap = true })
-            vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<C-v>k:VBox<CR>', { noremap = true })
-            vim.api.nvim_buf_set_keymap(0, 'n', 'L', '<C-v>l:VBox<CR>', { noremap = true })
-            vim.api.nvim_buf_set_keymap(0, 'n', 'H', '<C-v>h:VBox<CR>', { noremap = true })
-            -- draw a box by pressing "f" with visual selection
-            vim.api.nvim_buf_set_keymap(0, 'v', 'f', ':VBox<CR>', { noremap = true })
-          else
-            vim.cmd([[setlocal ve=]])
-            vim.api.nvim_buf_del_keymap(0, 'n', 'J')
-            vim.api.nvim_buf_del_keymap(0, 'n', 'K')
-            vim.api.nvim_buf_del_keymap(0, 'n', 'L')
-            vim.api.nvim_buf_del_keymap(0, 'n', 'H')
-            vim.api.nvim_buf_del_keymap(0, 'v', 'f')
-            vim.b.venn_enabled = nil
-          end
-        end
-        -- toggle keymappings for venn using <leader>v
-        vim.api.nvim_set_keymap('n', '<leader>v', ':lua Toggle_venn()<CR>', { noremap = true })
-      end,
+        vim.keymap.set("n", "<leader>fu", vim.cmd.UndotreeToggle, { desc = "Undo history" })
+      end
     },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    },
+    -- LSP defaults
+    {
+      'neovim/nvim-lspconfig',
+    }
+
   },
 })
