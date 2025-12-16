8b23e6576cce418d5b64bd53b1175f742cf7c312
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-12-15 20:18:50 -0600

    feat: oil buffer view opts and winbar

--- lua/mocos/plugins/oil.lua
+++ lua/mocos/plugins/oil.lua
@@ -1,22 +1,36 @@
-return {
-  'stevearc/oil.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('oil').setup({
-      columns = { 'icon' },
-      keymaps = {
-        ['<C-h>'] = false,
-        ['<C-l>'] = false,
-        ['<C-k>'] = false,
-        ['<C-j>'] = false,
-        ['<M-h>'] = 'actions.select_split',
-      },
-      view_options = {
-        show_hidden = true,
-      },
-    })
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
 
-    -- Open parent directory in current window
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open in current directory' })
-  end,
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    columns = { "icon", "permissions", "size", "mtime" },
+    keymaps = {
+      ["<C-h>"] = false,
+      ["<C-l>"] = false,
+      ["<C-k>"] = false,
+      ["<C-j>"] = false,
+      ["<M-h>"] = "actions.select_split",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+    win_options = {
+      winbar = "%!v:lua.get_oil_winbar()",
+    },
+    default_file_explorer = true,
+  },
+  lazy = false,
+
+  -- Open parent directory in current window
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open in current directory" }),
 }
